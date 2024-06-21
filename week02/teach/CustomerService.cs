186d3c6a14d0391ddeba209ebd39465c72f55683
Borgesfacundo <borgesfacundo@gmail.com>
2024-06-20 23:14:46 -0300

    W02 team activity almost finished

--- week02/teach/CustomerService.cs
+++ week02/teach/CustomerService.cs
@@ -2,8 +2,10 @@
 /// Maintain a Customer Service Queue.  Allows new customers to be 
 /// added and allows customers to be serviced.
 /// </summary>
-public class CustomerService {
-    public static void Run() {
+public class CustomerService
+{
+    public static void Run()
+    {
         // Example code to see what's in the customer service queue:
         // var cs = new CustomerService(10);
         // Console.WriteLine(cs);
@@ -11,11 +13,29 @@ public class CustomerService {
         // Test Cases
 
         // Test 1
-        // Scenario: 
-        // Expected Result: 
+        // Scenario: Queue of 3 values ("Adam", "ABC", "card declined"), ("Sophia", "DEF", "Battery dead"), ("Carlos", "GHI", "Needs a translator")
+        // Expected Result: Adam, Sohpia, Carlos / Empty List
         Console.WriteLine("Test 1");
+        var cs = new CustomerService(3);
+        cs.AddNewCustomer();
+        cs.AddNewCustomer();
+        cs.AddNewCustomer();
+        //Check what is in the customerService()
+        Console.WriteLine(cs);
+        //Delete Adam
+        cs.ServeCustomer();
+        //Delete Sohpia
+        cs.ServeCustomer();
+        //Delete Carlos
+        cs.ServeCustomer();
 
-        // Defect(s) Found: 
+        //Check if Queue is empty
+        Console.WriteLine(cs);
+
+        cs.ServeCustomer();
+
+        // Defect(s) Found: in ServeCustomer() It was displaying the next customer in Queue and not the removed one. 
+        //And It was missing an if statment to verify if queue is empty to print an error message.
 
         Console.WriteLine("=================");
 
@@ -34,7 +54,8 @@ public class CustomerService {
     private readonly List<Customer> _queue = new();
     private readonly int _maxSize;
 
-    public CustomerService(int maxSize) {
+    public CustomerService(int maxSize)
+    {
         if (maxSize <= 0)
             _maxSize = 10;
         else
@@ -45,8 +66,10 @@ public class CustomerService {
     /// Defines a Customer record for the service queue.
     /// This is an inner class.  Its real name is CustomerService.Customer
     /// </summary>
-    private class Customer {
-        public Customer(string name, string accountId, string problem) {
+    private class Customer
+    {
+        public Customer(string name, string accountId, string problem)
+        {
             Name = name;
             AccountId = accountId;
             Problem = problem;
@@ -56,7 +79,8 @@ public class CustomerService {
         private string AccountId { get; }
         private string Problem { get; }
 
-        public override string ToString() {
+        public override string ToString()
+        {
             return $"{Name} ({AccountId})  : {Problem}";
         }
     }
@@ -65,9 +89,11 @@ public class CustomerService {
     /// Prompt the user for the customer and problem information.  Put the 
     /// new record into the queue.
     /// </summary>
-    private void AddNewCustomer() {
+    private void AddNewCustomer()
+    {
         // Verify there is room in the service queue
-        if (_queue.Count > _maxSize) {
+        if (_queue.Count > _maxSize)
+        {
             Console.WriteLine("Maximum Number of Customers in Queue.");
             return;
         }
@@ -87,10 +113,18 @@ public class CustomerService {
     /// <summary>
     /// Dequeue the next customer and display the information.
     /// </summary>
-    private void ServeCustomer() {
-        _queue.RemoveAt(0);
-        var customer = _queue[0];
-        Console.WriteLine(customer);
+    private void ServeCustomer()
+    {
+        if (_queue.Count == 0) //I added this if statment to verify if queue is empty to print an error message
+        {
+            Console.WriteLine("Queue is empty :D");
+        }
+        else
+        {
+            var customer = _queue[0];  //Here I need to get the customer deleted not the next customer. So I need to chmove this linne before the remove()
+            _queue.RemoveAt(0);
+            Console.WriteLine(customer);
+        }
     }
 
     /// <summary>
@@ -100,7 +134,8 @@ public class CustomerService {
     /// see the contents.
     /// </summary>
     /// <returns>A string representation of the queue</returns>
-    public override string ToString() {
+    public override string ToString()
+    {
         return $"[size={_queue.Count} max_size={_maxSize} => " + String.Join(", ", _queue) + "]";
     }
 }
