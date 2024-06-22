0607489f9523193007076a1f9595595eb6132caa
Borgesfacundo <borgesfacundo@gmail.com>
2024-06-22 17:35:26 -0300

    Completing w03 extra credit

--- week03/code/FeatureCollection.cs
+++ week03/code/FeatureCollection.cs
@@ -1,4 +1,18 @@
-public class FeatureCollection {
+public class FeatureCollection
+{
     // Todo Earthquake Problem - ADD YOUR CODE HERE
     // Create additional classes as necessary
+    public List<Feature> Features { get; set; }
+}
+
+public class Feature
+{
+    public Properties Properties { get; set; }
+}
+
+public class Properties
+{
+    public double Magnitude { get; set; }
+
+    public string Place { get; set; }
 }
