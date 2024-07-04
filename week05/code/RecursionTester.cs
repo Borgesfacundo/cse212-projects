7129449cca349ba0f210283a74e136380c3d18f9
Borgesfacundo <borgesfacundo@gmail.com>
2024-07-04 17:33:15 -0300

    Activity 3 solved

--- week05/code/RecursionTester.cs
+++ week05/code/RecursionTester.cs
@@ -67,9 +67,9 @@ public static class RecursionTester
         Console.WriteLine("\n=========== PROBLEM 3 TESTS ===========");
         Console.WriteLine(CountWaysToClimb(5)); // 13
         Console.WriteLine(CountWaysToClimb(20)); // 121415
-        // Uncomment out the test below after implementing memoization.  It won't work without it.
-        // TODO Problem 3
-        // Console.WriteLine(CountWaysToClimb(100));  // 180396380815100901214157639
+                                                 // Uncomment out the test below after implementing memoization.  It won't work without it.
+                                                 // TODO Problem 3
+        Console.WriteLine(CountWaysToClimb(100));  // 180396380815100901214157639
 
         // Sample Test Cases (may not be comprehensive) 
         Console.WriteLine("\n=========== PROBLEM 4 TESTS ===========");
@@ -250,9 +250,23 @@ public static class RecursionTester
         if (s == 3)
             return 4;
 
-        // Solve using recursion
-        decimal ways = CountWaysToClimb(s - 1) + CountWaysToClimb(s - 2) + CountWaysToClimb(s - 3);
-        return ways;
+        if (remember == null)
+            remember = new Dictionary<int, decimal>();
+
+        if (remember.ContainsKey(s))
+            return remember[s];
+
+        else
+        {
+            // Solve using recursion
+            decimal ways = CountWaysToClimb(s - 1, remember) + CountWaysToClimb(s - 2,remember) + CountWaysToClimb(s - 3,remember);
+            remember[s] = ways;
+            return ways;
+        }
+
+
+
+
     }
 
     /// <summary>
