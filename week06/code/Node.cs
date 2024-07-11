1f8f06507b9cc3f0435add925a169084f14dd045
Borgesfacundo <borgesfacundo@gmail.com>
2024-07-11 10:30:17 -0300

    Week 06 problem 1 and 2 fixed

--- week06/code/Node.cs
+++ week06/code/Node.cs
@@ -1,35 +1,61 @@
-public class Node {
+public class Node
+{
     public int Data { get; set; }
     public Node? Right { get; private set; }
     public Node? Left { get; private set; }
 
-    public Node(int data) {
+    public Node(int data)
+    {
         this.Data = data;
     }
 
-    public void Insert(int value) {
-        if (value < Data) {
+    public void Insert(int value)
+    {
+        if (value < Data)
+        {
             // Insert to the left
             if (Left is null)
                 Left = new Node(value);
-            else
+            else if (Left is not null && !Left.Contains(value))
                 Left.Insert(value);
         }
-        else {
+        else
+        {
             // Insert to the right
             if (Right is null)
                 Right = new Node(value);
-            else
+            else if (Right is not null && !Right.Contains(value))
                 Right.Insert(value);
         }
     }
 
-    public bool Contains(int value) {
+    public bool Contains(int value)
+    {
         // TODO Start Problem 2
-        return false;
+        if (value < Data)
+        {
+            if (Left is null)
+            {
+                return false;
+            }
+            return Left.Contains(value);
+        }
+
+        else if (value > Data)
+        {
+            if (Right is null)
+            {
+                return false;
+            }
+            return Right.Contains(value);
+        }
+        else {
+            return true;
+        }
     }
 
-    public int GetHeight() {
+    public int GetHeight()
+    {
         // TODO Start Problem 4
         return 0; // Replace this line with the correct return statement(s)
     }
